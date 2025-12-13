8bf8082e22cf4256963e7fa765ea661799e661a5
Adam Farahat <farahatadam@gmail.com>
2025-12-13 12:50:52 -0500

    Connect Right Panel to Library

---
+++ Assets/Scripts/EbookContent.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class EbookContent : MonoBehaviour
+{
+    public TextMeshProUGUI ebookText;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RightPanelManager.onReadButtonPressed.AddListener(UpdateEbookContent);
+    }
+
+    void UpdateEbookContent()
+    {
+        if (ebookText != null)
+        {
+            ebookText.text = BookManager.instance.bookText;
+            Debug.Log("Ebook content updated.");
+        }
+        else
+        {
+            Debug.LogWarning("EbookContent: No TextMeshProUGUI component assigned.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
